1ddbe76e836c138ccaf680b07b1df7778b4c1f26
Satyajit   Ray <roysatyajit772912@gmail.com>
2023-03-26 17:43:32 +0530

    Update CorsFilter.java

--- src/main/java/com/masai/model/CorsFilter.java
+++ src/main/java/com/masai/model/CorsFilter.java
@@ -1,28 +1,28 @@
-// package com.masai.model;
+package com.masai.model;
 
-// import org.springframework.core.Ordered;
-// import org.springframework.core.annotation.Order;
-// import org.springframework.stereotype.Component;
-// import javax.servlet.*;
-// import javax.servlet.http.HttpServletResponse;
-// import java.io.IOException;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-// @Component
-// @Order(Ordered.HIGHEST_PRECEDENCE)
-// public class CorsFilter implements Filter {
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsFilter implements Filter {
 
-//     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-//         HttpServletResponse response = (HttpServletResponse) res;
-//         response.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5501");
-//         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-//         response.setHeader("Access-Control-Allow-Headers", "authorization, content-type");
-//         response.setHeader("Access-Control-Max-Age", "3600");
-//         chain.doFilter(req, res);
-//     }
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "https://ziptip-frontend-production.up.railway.app/");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Headers", "authorization, content-type");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        chain.doFilter(req, res);
+    }
 
-//     public void init(FilterConfig filterConfig) {}
+    public void init(FilterConfig filterConfig) {}
 
-//     public void destroy() {}
+    public void destroy() {}
 
-// }
+}
 
